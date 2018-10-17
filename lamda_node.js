6aa817c3dfb113038c2da0e975995dfc10a761d2
Shruthi Tandel <shruthitandel@gmail.com>
2018-10-17 11:49:57 -0400

    Made changes to user messages.

--- lamda_node.js
+++ lamda_node.js
@@ -10,22 +10,34 @@ exports.handler = async (event,context) => {
     
     switch (event.key1) {
     case "Hi":
+	case "hi":
+	case "hey":
+	case "Hey":
         console.log("Hi, How can I help you ?");
         responsecode = "Hi, How can I help you ?"
         break;
     case "Hello":
+	case "hello":
          responsecode ="Hello, How are you";
         break;
     case "Who are you":
+	case "who are you":
         responsecode = "I am Chatbot";
         break;
      case "Bye":
+	 case "bye":
+	 case "See you":
+	 case "see you":
         responsecode = "See you again";
         break;
      case "Thank you":
+	 case "thank you":
+	 case "thanks":
+	 case "Thanks":
         responsecode = "You are welcome, have a good day!";
         break;  
     case "what is your name":
+	case "What's your name":
         responsecode = "I am chatbot";
         break;      
     default:
